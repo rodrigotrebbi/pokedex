bee8274296760fe0af51f29f248c88461f2f522e
RodrigoTrebbi <rodrigo_trebbi@hotmail.com>
2025-04-01 17:22:50 -0300

    doglist

---
+++ app/dogapi/page.js
@@ -0,0 +1,16 @@
+'use client'
+function Dogs(){
+    
+    const [dogs, alteradogs]
+
+
+    return(
+        <div className="px-5">
+            <h1> Lista de Doguinhos </h1>
+
+
+        </div>
+    );
+}
+
+export default Dogs
