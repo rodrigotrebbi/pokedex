b4f51337b3a0e6d5f4ec5a58b610c50d708daccf
RodrigoTrebbi <rodrigo_trebbi@hotmail.com>
2025-04-02 16:03:37 -0300

    listadogs

--- app/dogapi/page.js
+++ app/dogapi/page.js
@@ -1,16 +1,84 @@
-'use client'
-function Dogs(){
+    'use client'
+
+    import axios from "axios";
+    import { useEffect, useState } from "react";
+
+    function Dogs() {
+
+    const [ dogs, alteraDogs ] = useState([])
+    const [ racas,alteraRacas] = useState([])
+
+    async function buscaTodosDogs(){
+        const response = await axios.get("https://dog.ceo/api/breeds/image/random/30")
+        alteraDogs( response.data.message )
+    }   
+
+    async function buscaPorRaca( raca ){
+        const response = await axios.get("https://dog.ceo/api/breed/"+raca+"/images")
+        alteraDogs( response.data.message )
+    }
+
+    async function buscaTodasRacas(){
+        const response = await axios.get("https://dog.ceo/api/breeds/list/all")
+        alteraRacas( Object.keys( response.data.message) )
+    }
+
+    useEffect(()=> {
+        buscaTodosDogs()
+        buscaTodasRacas()
+    }, [])
+
+
+
+
+            return (
+        
+                <div className="px-20">
+            
+                <h1 className="p-10 mb-10 text-center text-green-700 bg-green-50 text-2xl" >Lista de Doguinhos</h1>
+                <p>Os melhores hotdogs estão aqui:</p>
     
-    const [dogs, alteradogs]
+                <hr/>
 
+                <div className="flex">
 
-    return(
-        <div className="px-5">
-            <h1> Lista de Doguinhos </h1>
+                    
+                    
+                    <div>
+                        <button onClick={()=> buscaTodosDogs() } className="bg-green-200 text-green-700 m-3 mx-1 p-3">Mostrar tudo</button>
 
+                        {
+                            
+                            racas.length > 0 &&
+                            racas.map( i => 
+                            <button onClick={()=> buscaPorRaca(i) } className="bg-green-200 text-green-700 m-3 mx-1 p-3"> {i} </button>)
+                           
+                        }
 
-        </div>
-    );
-}
+                        
+                    </div>
 
-export default Dogs
+     
+                       
+                {
+                    dogs.length > 0 ?
+                        <div className="flex gap-5 flex-wrap" >
+                            {
+                                dogs.map( i => 
+                                    <img src={i} width={100} />
+                                )
+                            }
+                        </div>
+                    :
+                        <p>Carregando...</p>
+                }
+                        
+                </div>
+    
+                
+    
+            </div>
+        );
+    }
+    
+    export default Dogs;
